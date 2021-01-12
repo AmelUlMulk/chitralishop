0b47ab7dff4267f0473e10f762eff637386e3fe7
Shahzada Sultan Ul Mulk <amelulmulk@outlook.com>
2021-01-12 10:17:53 +0500

    removed history

--- .history/client/src/index_20201127102847.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./bootstrap.min.css";
-import "./fontawesome-free-5.12.1-web/css/all.min.css";
-
-import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
--- .history/middleware/async_20201229070215.js
+++
@@ -1,5 +0,0 @@
-//middle ware to handle async functions to save us from try catch block 
-const asyncHandler = (fn) => (req, res, next) =>
-  Promise.resolve(fn(req, res, next)).catch(next);
-
-export default asyncHandler;
